c7ac443ec49ece72e2ddcdc6f23ce4a0b451b15a
Losharik <tankustvotnycke@yandex.ru>
2021-07-18 17:48:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from app import current_app, db
+from app import create_app, db
 from app.models import User, Expert, Grade
 
 
-@current_app.shell_context_processor
+app = create_app()
+
+@app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Expert': Expert, 'Grade': Grade}
 
