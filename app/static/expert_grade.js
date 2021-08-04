8fe8287b484d9492527ea456f920ca7fd38f8b5d
Losharik <tankustvotnycke@yandex.ru>
2021-08-05 01:04:17 +0300

    Update expert_grade.js

--- app/static/expert_grade.js
+++ app/static/expert_grade.js
@@ -1,3 +1,22 @@
 $('#submit').addClass('form_button')
 $('#comment').addClass('comment')
 $('#comment').attr('placeholder', 'Комментарий к оценке')
+
+// отмена оценки при втором клике
+$(":input").on("click", function (e) {
+    e.preventDefault();
+
+    setTimeout(
+        () => $(this).prop("checked", !this.checked).trigger("change")
+    );
+});
+
+// скрытие нулевых полей
+$(`[value='0']`).each(function (index, element) {
+    $(element).css('display', 'none')
+})
+
+$(`[for^='parameter']`).each(function (index, element) {
+    if ($(element).html() === '')
+        $(element).css('display', 'none')
+})
