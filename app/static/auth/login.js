be163e4174ca1fd2db3896bd71b311ac92b3030c
TorinLight <tujh.sad@gmail.com>
2021-08-15 23:50:40 +0300

    мелочь

--- app/static/auth/login.js
+++ app/static/auth/login.js
@@ -3,7 +3,6 @@ $('#remember_me').addClass('custom-checkbox')
 $('label[for=email]').addClass('form_label')
 $('label[for=password]').addClass('form_label')
 $('#email').addClass('form_input')
-
 $('#email').focus(function () {
     foc(this, $('label[for=email]'))
 }).blur(function () {
@@ -16,6 +15,9 @@ $('#password').focus(function () {
 }).blur(function () {
     blu(this, $('label[for=password]'))
 })
+
+
+
 
 function foc(el, label) {
     $(el).css({
