e632bfef4acaaacd145b2b3be50f821b35d8664a
TorinLight <tujh.sad@gmail.com>
2021-09-01 22:14:40 +0300

    Правки

--- app/static/auth/add_new.js
+++ app/static/auth/add_new.js
@@ -54,3 +54,21 @@ function blu(el, label) {
         })
 }
 
+
+let inputs = document.querySelectorAll('.input__file');
+Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input__file_button_text_2').innerText;
+
+    input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+            countFiles = this.files.length;
+
+        if (countFiles)
+            label.querySelector('.input__file_button_text_2').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+            label.querySelector('.input__file_button_text_2').innerText = labelVal;
+    });
+});
+
